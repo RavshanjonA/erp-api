6ce6455af1da6f32afa4f12f17b1e8f59e54be38
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2023-04-15 10:51:43 +0500

    pep8

--- apps/attandance/api_endpoints/attandance/AttandanceDetial/views.py
+++ apps/attandance/api_endpoints/attandance/AttandanceDetial/views.py
@@ -10,10 +10,11 @@ from apps.employee.models import Employee
 
 
 class AttandanceDetailView(APIView):
-    def get(self,request, pk):
-        employee  = get_object_or_404(Employee, pk = pk)
+    def get(self, request, pk):
+        employee = get_object_or_404(Employee, pk=pk)
         attandance = Attandance.objects.filter(employee=employee)
         serializer = AttandanceDetailSerailizer(attandance, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
 
 __all__ = ['AttandanceDetailView']
